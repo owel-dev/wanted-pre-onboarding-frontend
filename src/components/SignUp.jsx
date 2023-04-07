24367b78f3489406e50d3cc30d7df997deba0e45
usanglee <mail@usanglee.com>
2023-04-07 21:17:43 +0900

    refactor: SignUp: handleSubmit 함수 Error 부분 함수로 분리

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
 import { Link } from 'react-router-dom';
 import FormInput from './FormInput';
 import Form from './Form';
+import { handleError } from '../handleSubmit';
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -17,50 +16,36 @@ const SignUp = () => {
 
   const handleSubmit = async (formInputData) => {
     try {
-      const response = await axios.post(
+      await axios.post(
         'https://www.pre-onboarding-selection-task.shop/auth/signup',
         formInputData
       );
-      if (response.status === 201) {
-        alert('회원가입이 완료되었습니다.');
-        navigate('/signin');
-      } else {
-        alert(
-          `회원가입이 정상 처리되지 않았습니다. 에러코드: ${response.status}`
-        );
-      }
+      alert('회원가입이 완료되었습니다.');
+      navigate('/signin');
     } catch (error) {
-      const keyValueString = Object.entries(error.response.data)
-        .map(([key, value]) => `${key}: ${value}`)
-        .join(',\n');
-      alert(`회원가입에 실패했습니다.\n${keyValueString}`);
+      handleError(error, '회원가입');
     }
   };
 
   return (
     <div className="signup">
       <h1>회원가입</h1>
-      <Form
-        handleSubmit={handleSubmit}
-        initialValues={initialValues}
-        buttonName="회원가입"
-      >
-        <>
-          <FormInput
-            label="이메일"
-            type="text"
-            name="email"
-            pattern=".*@.*"
-            title="이메일은 '@' 기호를 포함해야 합니다."
-          />
-          <FormInput
-            label="비밀번호"
-            type="password"
-            name="password"
-            pattern=".{8,}"
-            title="비밀번호는 8자 이상이어야 합니다."
-          />
-        </>
+      <Form handleSubmit={handleSubmit} initialValues={initialValues}>
+        <FormInput
+          label="이메일"
+          type="text"
+          name="email"
+          pattern=".*@.*"
+          title="이메일은 '@' 기호를 포함해야 합니다."
+        />
+        <FormInput
+          label="비밀번호"
+          type="password"
+          name="password"
+          pattern=".{8,}"
+          title="비밀번호는 8자 이상이어야 합니다."
+        />
+        <button type="submit">회원가입</button>
       </Form>
       <br />
       <Link to="/">홈</Link> &nbsp;&nbsp;
