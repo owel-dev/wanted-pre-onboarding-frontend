fb929ba50b7b90548a948d34fca1f68714ffaf57
usanglee <mail@usanglee.com>
2023-04-07 21:17:13 +0900

    refactor: SignIn: handleSubmit 함수 Error 부분 함수로 분리

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,6 +5,7 @@ import Cookies from 'js-cookie';
 import { Link } from 'react-router-dom';
 import FormInput from './FormInput';
 import Form from './Form';
+import { handleError } from '../handleSubmit';
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -20,45 +21,33 @@ const SignIn = () => {
         'https://www.pre-onboarding-selection-task.shop/auth/signin',
         formInputData
       );
-      if (response.status === 200) {
-        const { access_token } = response.data;
-        Cookies.set('access_token', access_token);
-        navigate('/todo');
-      } else {
-        alert('로그인이 정상 처리되지 않았습니다.');
-      }
+      const { access_token } = response.data;
+      Cookies.set('access_token', access_token);
+      navigate('/todo');
     } catch (error) {
-      const keyValueString = Object.entries(error.response.data)
-        .map(([key, value]) => `${key}: ${value}`)
-        .join(',\n');
-      alert(`로그인에 실패했습니다.\n${keyValueString}`);
+      handleError(error, '로그인');
     }
   };
 
   return (
     <div className="signin">
       <h1>로그인</h1>
-      <Form
-        handleSubmit={handleSubmit}
-        initialValues={initialValues}
-        buttonName="로그인"
-      >
-        <>
-          <FormInput
-            label="이메일"
-            type="text"
-            name="email"
-            pattern=".*@.*"
-            title="이메일은 '@' 기호를 포함해야 합니다."
-          />
-          <FormInput
-            label="비밀번호"
-            type="password"
-            name="password"
-            pattern=".{8,}"
-            title="비밀번호는 8자 이상이어야 합니다."
-          />
-        </>
+      <Form handleSubmit={handleSubmit} initialValues={initialValues}>
+        <FormInput
+          label="이메일"
+          type="text"
+          name="email"
+          pattern=".*@.*"
+          title="이메일은 '@' 기호를 포함해야 합니다."
+        />
+        <FormInput
+          label="비밀번호"
+          type="password"
+          name="password"
+          pattern=".{8,}"
+          title="비밀번호는 8자 이상이어야 합니다."
+        />
+        <button type="submit">로그인</button>
       </Form>
       <br />
       <Link to="/">홈</Link> &nbsp;&nbsp;
